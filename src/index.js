5c381c52a5ba42243a3349b11b22cc809dce90ac
Cynthia Teeters <glean@cynthiateeters.com>
2021-04-10 15:57:47 -0400

    Init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import wikiResults from './api-result.json';
+
+/* 
+
+Put code here to add the first snippet from api-result.json 
+
+Since the snippet is written as HTML, you will use innerHTML
+
+
+*/
